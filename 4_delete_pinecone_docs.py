da467666333568d891e367f916337e45f8d11044
FTC23368 <malihajburney@gmail.com>
2025-07-13 17:15:34 -0700

    Cleaned up 4_delete_pinecone_docs

--- 4_delete_pinecone_docs.py
+++ 4_delete_pinecone_docs.py
@@ -1,16 +1,10 @@
 # src/template_agent.py
 import streamlit as st
-from typing import List
-from pydantic import BaseModel
-from src.create_llm_message import create_llm_message, create_llm_msg
-from langchain_core.messages import BaseMessage
 from pinecone import Pinecone
-
 
 pinecone_api_key = st.secrets['PINECONE_API_KEY']
 pinecone_env = st.secrets['PINECONE_API_ENV']
 pinecone_index_name = st.secrets['PINECONE_INDEX_NAME']
 pinecone = Pinecone(api_key=pinecone_api_key)
 index = pinecone.Index(pinecone_index_name)
-
 print(index.delete(filter={"docname": "spif_design_prompt (1).md"}))
